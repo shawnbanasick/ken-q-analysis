90ad479eb123e4e95486d50c160b175c0eda10ae
Shawn Banasick <shawnbanasick@users.noreply.github.com>
2018-11-14 17:20:06 +0900

    11-14 update

--- src/S0-info/IntroText/IntroText.jsx
+++ src/S0-info/IntroText/IntroText.jsx
@@ -9,21 +9,21 @@ const styles = {
 class IntroText extends Component {
   render() {
     return (
-      <div style={styles}>
+      <div style={ styles }>
         <span>
-          Ken-Q Analysis brings the interactivity and convenience of the web to
-          Q-methodology. It is a desktop web application, so after the page has
-          loaded there is no further communication with the server. All matrix
-          calculations, factor rotations, and file downloads are processed in
-          your browser.{" "}
-          <strong>Your data never leaves your web browser. </strong>
-          Therefore, the speed of the matrix calculations, table updates, and
-          image displays will depend on the processing power of your computer.
-          Analyzing smaller datasets will likely be quick and responsive, but
-          when working with large datasets you may experience some delays.
-        </span>
+                Ken-Q Analysis brings the interactivity and convenience of the web to
+                Q-methodology. It is a desktop web application, so after the page has
+                loaded there is no further communication with the server. All matrix
+                calculations, factor rotations, and file downloads are processed in
+                your browser.{ " " }
+                <strong>Your data never leave your web browser. </strong>
+                Therefore, the speed of the matrix calculations, table updates, and
+                image displays will depend on the processing power of your computer.
+                Analyzing smaller datasets will likely be quick and responsive, but
+                when working with large datasets you may experience some delays.
+              </span>
       </div>
-    );
+      );
   }
 }
 
--- src/S0-info/OtherTools/ReferenceManualDownloadButton.jsx
+++ src/S0-info/OtherTools/ReferenceManualDownloadButton.jsx
@@ -3,13 +3,8 @@ import { Button } from "semantic-ui-react";
 
 const ReferenceManualDownloadButton = () => (
   <div>
-    <a
-      href="https://www.gitbook.com/book/ken_q_tools/ken-q-analysis-reference-guide/details"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ targetNew: "tab" }}
-    >
-      <Button id="referenceGuideLink" style={{ margin: 20, opacity: 0.9 }} basic size="huge">
+    <a href="https://ken_q_tools.gitbooks.io/ken-q-analysis-reference-guide/content/" target="_blank" rel="noopener noreferrer" style={ { targetNew: "tab" } }>
+      <Button id="referenceGuideLink" style={ { margin: 20, opacity: 0.9 } } basic size="huge">
         <strong>Reference Manual</strong>
       </Button>
     </a>

cf196bd36fa969185aadf4173254b516b5e94ccc
Shawn Banasick <shawnbanasick@users.noreply.github.com>
2018-11-14 17:07:51 +0900

    11-14 update

---
+++ src/__tests__/S1-data_TESTS/Excel_TESTS/calcMultiplierArray.test.js
@@ -0,0 +1,12 @@
+
+import calcMultiplierArray from "../../../S1-data/UploadFile/excelUploadLogic/calcMultiplierArrayT2";
+
+const parameter1 = [-4, -4, -3, -3, -3, -2, -2, -2, -2, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4];
+
+const testValue1 = [0, 0, 2, 3, 4, 5, 5, 5, 4, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+test("calc multiplier array", () => {
+    let value1 = calcMultiplierArray(parameter1);
+    expect(value1).toEqual(testValue1);
+});
+

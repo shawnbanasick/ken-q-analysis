08aa62e3b679754a60ef9b5d91945f82fd610abd
banasick <banasick@mail.kobe-c.ac.jp>
2026-08-16 10:32:18 +0900

    updated zustand and ajv

--- src/S0-info/OtherTools/EqWebSortButton.tsx
+++ src/S0-info/OtherTools/EqWebSortButton.tsx
@@ -3,12 +3,8 @@ import OnGrayGeneralButton from "../../ReusableComponents/OnGrayGeneralButton";
 
 const EasyHtmlButton = () => (
   <div className="flex flex-row justify-center w-[280px]">
-    <a
-      href="https://github.com/shawnbanasick/eq-web-sort"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <OnGrayGeneralButton id="eqWebSortLink" buttonText="EQ Web Sort" />
+    <a href="https://quince-config.netlify.app/" target="_blank" rel="noopener noreferrer">
+      <OnGrayGeneralButton id="eqWebSortLink" buttonText="Quince" />
     </a>
   </div>
 );
